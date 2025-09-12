d3978dcc7d0ed513d59fc6f2e00a8e55a0572995
VyVictory <vyvictory2003@gmail.com>
2025-09-13 00:39:25 +0700

    build

--- build/app.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = appConfig;
-const Match3Room_1 = require("./rooms/Match3Room");
-function appConfig(gameServer) {
-    gameServer.define("match3", Match3Room_1.Match3Room);
-}
-;
