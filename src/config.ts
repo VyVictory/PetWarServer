f5e5c636b4865de42e1160ff602065c06ec9daa3
VyVictory <vyvictory2003@gmail.com>
2025-09-13 00:39:16 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config(); 
 export const CONFIG = {
-    PORT: process.env.PORT ? Number(process.env.PORT) : 1000,
+    PORT: process.env.PORT ? Number(process.env.PORT) : 443,
     JWT_SECRET: process.env.JWT_SECRET || "a96cbf028870c29249f83fbe0d75667df9398e03",
     MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
     DB_NAME: process.env.DB_NAME || "petwar_db",
