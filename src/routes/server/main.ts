e1722b1d24421228327bc5d46b7ea492960aef9d
VyVictory <vyvictory2003@gmail.com>
2025-09-15 13:52:55 +0700

    Update main.ts

--- src/routes/server/main.ts
+++ src/routes/server/main.ts
@@ -2,8 +2,24 @@ import { Router } from "express";
 
 const router = Router();
 
+// Ping test
 router.get("/ping", (_req, res) => {
     res.send("pong");
+});
+ 
+router.get("/list", (req, res) => {
+    const serverInfo = [
+        {
+            name: "Server Test", // đổi theo tên bạn muốn
+            ip: "req.hostname",          // hostname từ request
+            address: "http://localhost:2567"
+        }, {
+            name: "Trái đất", // đổi theo tên bạn muốn
+            ip: "req.hostname",          // hostname từ request
+            address: "https://petwarserver.onrender.com"
+        }
+    ] 
+    res.json(serverInfo);
 });
 
 export default router;
