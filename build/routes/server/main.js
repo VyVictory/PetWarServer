b99739b0039c0ab4c90133d12855e04086900a02
VyVictory <vyvictory2003@gmail.com>
2025-09-15 14:18:50 +0700

    Update main.js

--- build/routes/server/main.js
+++ build/routes/server/main.js
@@ -2,7 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
+// Ping test
 router.get("/ping", (_req, res) => {
     res.send("pong");
+});
+router.get("/list", (req, res) => {
+    const serverInfo = [
+        {
+            name: "Server Test", // đổi theo tên bạn muốn
+            ip: "req.hostname", // hostname từ request
+            address: "http://localhost:2567"
+        }, {
+            name: "Trái đất", // đổi theo tên bạn muốn
+            ip: "req.hostname", // hostname từ request
+            address: "https://petwarserver.onrender.com"
+        }
+    ];
+    res.json(serverInfo);
 });
 exports.default = router;
